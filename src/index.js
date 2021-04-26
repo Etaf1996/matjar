2ceb05106cceb9108bfd9a5cd30f2ef5e714a815
hsoubecommerce <hsoubecommerce@gmail.com>
2021-04-26 18:19:59 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,115 @@
-import './style.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import '@fortawesome/fontawesome-free/js/all.min';
+import 'bootstrap';
+
+$(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    $(".add-to-cart-btn").on("click" , function(){
+     alert("أضيف المنتج إلي عربة الشراء");
+    });
+    $("#copyright").text("  جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear());
+    $('.product-option input[type="radio"]').change(function() {
+      $(this).parents(".product-option").siblings().removeClass("active");
+      $(this).parents(".product-option").addClass("active");
+    });
+  $("[data-product-quantity]").change(function() {
+    // عدد المنتجات
+    var newQuantity = $(this).val();
+    // البحث عن السطر الذي يحتوي على معلومات المنتج
+    var parent = $(this).parents("[data-product-info]");
+    // سعر المنتج الواحد
+    var pricePerUnit = parent.attr("data-product-price");
+    // اجمالى سعر المنتجات
+    var totalPriceForProduct = newQuantity * pricePerUnit ;
+    parent.find(".total-price-for-product").text(totalPriceForProduct + "$"); 
+    calculate();
+  });
+  
+  
+function calculate() {
+  // انشيء متغير جديد لحفظ السعر الأجمالي
+var totalPriceForAll = 0;
+// لكل سطر يمثل معلومات المنتج في الصفحة   
+$("[data-product-info]").each(function(){
+// سعر القطعة الواحدة فى السطر
+var pricePerUnit = $(this).attr("data-product-price");
+// عدد المنتجات فى السطر الواحد
+var quantity = $(this).find("[data-product-quantity]").val();
+// اجمالي سعر المنتج الواحد
+var totalPriceForProduct = pricePerUnit * quantity;
+// سعر الاجمالي للمنتجات كلها عن طريق السعر الاجمالي للمنتج +سعر المنتجات كلها التى قيمتها الافتراضية =0
+// totalPriceForAll = totalPriceForAll + totalPriceForProduct;
+totalPriceForAll+= totalPriceForProduct;
+$("#total-price-for-all").text(totalPriceForAll + "$");
+
+});
+}
+$("[data-remove-from-cart]").on("click" , function(){
+  // تحديد السطر المراد حذفه
+  $(this).parents("[data-product-info]").remove();
+  calculate() ;
+});
+var citiesByCountry = {
+  sa:["الرياض" , "جدة"],
+  eg:["القاهرة" , "الإسكندرية"],
+  jo:["عمان" , "الزرقاء"],
+  sy:["دمشق" , "حلب" , "حماة"]
+}
+                                // عندما يتغير البلد
+$("#form-checkout select[name = 'country']").change(function() {
+              // أجلب أسم الدولة
+          var country = $(this).val();
+          // جلب مدن هذا البلد من المصفوفة
+        var cities =   citiesByCountry[country];
+                  // تفريغ قائمة المدن
+        $("#form-checkout select[name = 'city']").empty();
+                        // أضافة خيار أختر مدينة
+        $("#form-checkout select[name = 'city']").append(
+          "<option value= ' ' disabled selected>أختر المدينة</option>"
+        );
+                                // أضف المدن إلي قائمة المدن
+        cities.forEach(function(city) {
+          var newOption = $("<option></option>");
+          newOption.text(city);
+          newOption.val(city);
+          $("#form-checkout select[name = 'city']").append(newOption);
+        });
+});
+  $("#form-checkout input[name='payment_method']").change(function() {
+              // جلب القيمة المختارة حاليا
+    var paymentMethod = $(this).val();
+    if(paymentMethod =="on_delivery"){
+
+      $("#credit-card-info input").prop("disabled" , true);
+
+    }
+
+    else{
+
+      $("#credit-card-info input").prop("disabled" , false);
+
+    }
+        // تبديل معلومات البطاقة
+    $("#credit-card-info").toggle();
+
+  });
+  $( "#price-range" ).slider({
+    range: true,
+    min: 50,
+    max: 1000,
+    step:50,
+    values: [ 250, 800 ],
+    slide: function( event, ui ) {
+      $("#price-min").text(ui.values[ 0 ]);
+      $("#price-max").text(ui.values[1]);
+    }
+  });
+
+});
 
 
- 
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,74 +7,80 @@ const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 module.exports = {
   entry:  {
     app:'./src/index.js'
-    },
-    output: {
-      path: path.join(__dirname, "/dist"),
-      publicPath: '',
-      filename: "main.js"
-    },
-    mode: "development",
-    
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    publicPath: '',
+    filename: "main.js"
+  },
+  mode: "development",   
   devServer: {
     contentBase: path.join(__dirname, "/dist"),
     port: 1233,
     overlay: true,//for errors
     writeToDisk: true,
+    open:true
   },
-
-
-    module: {
-        rules: [
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
           {
-            test: /\.html$/,
-            use: [
-              {
-                
-loader: "html-loader",
-
-options: {
-
-minimize: true,
-
-}
-
-}
-
-]
-
-},
-
-{
-
-
-
-
-test:/\.css$/,
-
-use:[
-
-'style-loader',
-MiniCssExtractPlugin.loader,
-
-'css-loader'
-
-]
-
-},
-{
-  test: /\.(png|svg|jpe?g|gif)$/,
-  use: [
-    {
-      loader: "file-loader",
-      options: {
-        name: "[name].[ext]",
-        outputPath: "images",
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            }
+          }
+        ]
       },
+      
+      {
+        test:/\.css$/,
+        use:[
+          {
+            loader: MiniCssExtractPlugin.loader, 
+            options: {
+              publicPath: '../' 
+            },
+          },
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        use: [
+          {
+            loader: "file-loader", 
+            options: {
+              name: '[name].[ext]',
+              outputPath: "fonts",
+              esModule: false,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+      {
+        test: require.resolve('jquery'),
+        loader: 'expose-loader',
+        options: {
+          exposes: ['$', 'jQuery'],
+        }
+      },
+    ]
     },
-  ],
-},
-],
-},
   
       plugins: [
           
@@ -82,15 +88,11 @@ MiniCssExtractPlugin.loader,
           filename: "index.html",
           template: "./src/index.html",
         }),
-        
-        new MiniCssExtractPlugin ({filename:"style.css" }),
-        
-
+        new MiniCssExtractPlugin ({filename:"css/style.css" }),
 ],
 
     
     };
-    
     
 
 
